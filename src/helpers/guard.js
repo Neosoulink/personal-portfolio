22f339d8ccce2856d50ca55b0891897e1b68be8c
Neosoulink <neodarksoulink@gmail.com>
2021-01-02 18:31:24 +0100

    firebase firestore implementation

--- src/helpers/guard.js
+++ src/helpers/guard.js
@@ -1,9 +1,6 @@
 import vue from 'vue';
-import * as firebase from 'firebase/app';
 
 export function initialize(store, router, fire) {
-
-	firebase.init();
 
 	firebase.firestore.collection('skills').get().then(ref => {
 		store.commit('author/SET_SKILLS', ref.docs)
@@ -28,7 +25,7 @@ export function initialize(store, router, fire) {
 		return response;
 	}, error => {
 		if (error.response.status == 404) {
-			router.push('/404');
+			router.replace('/404');
 		}
 		return Promise.reject(error);
 	});
