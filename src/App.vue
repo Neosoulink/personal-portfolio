db6f4e7f39d67bc681477f9265f97d7745aa0b6c
Neosoulink <neodarksoulink@gmail.com>
2021-01-03 23:39:03 +0100

    Implementation of danger theme

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-	<div id="app">
+	<div id="app" class="dangerTheme">
 		<!-- Animated Background -->
 		<div class="img-animated-bg" :style="{transform: imgAnimatedBgTransformStyle}">
 			<vue-particles
