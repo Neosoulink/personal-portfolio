9b2e5cc3dd559169e6aacb87e27b8a78a804177a
Neosoulink <neodarksoulink@gmail.com>
2021-01-03 21:09:14 +0100

    passing skills data directly from initial state

--- src/store/modules/author.js
+++ src/store/modules/author.js
@@ -1,5 +1,15 @@
 const state = () => ({
-	skills: [],
+	skills: [
+		{ title: "JS(ES6-ES2020)", percentage: 70 },
+		{ percentage: 70, title: "Laravel" },
+		{ percentage: 75, title: "ReactNative" },
+		{ title: "HTLM, CSS", percentage: 75 },
+		{ percentage: 65, title: "Nodejs" },
+		{ percentage: 85, title: "Vuejs (& nuxtjs)" },
+		{ title: "WordPress", percentage: 65 },
+		{ title: "React (& nextjs)", percentage: "65" },
+		{ percentage: "80", title: "PHP(Poo)" }
+	],
 });
 
 const getters = {
