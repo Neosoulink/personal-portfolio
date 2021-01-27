91e11129f91dfc7759ea0c8f9c4030e5822c8753
Neosoulink <neodarksoulink@gmail.com>
2021-01-27 19:06:15 +0100

    'lost

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -3,10 +3,415 @@
 	<section data-id="portfolio" class="portfolio animated-section">
 		<div class="section-content">
 			<div class="parallax-parent">
-
 				<section class="parallax-child-container">
-					<div class="parallax-child">
-						<p>MY PROJECTS</p>
+					<div class="parallax-child slider js-slider">
+						<div class="slider__container js-container" data-scroll>
+							<div class="slider__item js-item" style="--aspect-ratio: 0.8">
+								<div
+									class="slider__item-img-wrap js-img-wrap js-img"
+									style="--aspect-ratio: 0.8"
+									data-building="building-slug"
+									data-index="1"
+								>
+									<img
+										src="https://images.unsplash.com/photo-1479839672679-a46483c0e7c8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80&ar=0.8"
+										alt="building"
+										class="slider__item-img"
+									/>
+								</div>
+
+								<div class="slider__item-content">
+									<div class="slider__item-heading-wrap">
+										<h3 class="slider__item-heading js-heading">Indigo</h3>
+									</div>
+
+									<div class="slider__item-button-wrap">
+										<button
+											class="button slider__item-button js-button"
+											type="button"
+											data-slug="indigo"
+										>
+											<span class="button__title-mask">
+												<div class="button__title-wrap">
+													<span class="button__title">Read more</span>
+													<span class="button__title--secondary"
+														>Read more</span
+													>
+												</div>
+											</span>
+
+											<span class="button__icon">
+												<span class="button__circle"></span>
+												<svg
+													class="button__svg"
+													xmlns="http://www.w3.org/2000/svg"
+													viewBox="0 0 16.002 4.639"
+												>
+													<path d="M.25 2.264h15.403" />
+													<path d="M13.743.354l1.91 1.91-1.919 2.022" />
+												</svg>
+												<svg
+													class="button__svg button__svg--clone"
+													xmlns="http://www.w3.org/2000/svg"
+													viewBox="0 0 16.002 4.639"
+												>
+													<path d="M.25 2.264h15.403" />
+													<path d="M13.743.354l1.91 1.91-1.919 2.022" />
+												</svg>
+											</span>
+										</button>
+									</div>
+								</div>
+							</div>
+
+							<div class="slider__item js-item" style="--aspect-ratio: 0.8">
+								<div
+									class="slider__item-img-wrap js-img-wrap js-img"
+									style="--aspect-ratio: 0.8"
+									data-building="building-slug"
+									data-index="1"
+								>
+									<img
+										src="https://images.unsplash.com/photo-1566688342604-dbe3e7357104?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80&ar=0.8"
+										alt="building"
+										class="slider__item-img"
+									/>
+								</div>
+
+								<div class="slider__item-content">
+									<div class="slider__item-heading-wrap">
+										<h3 class="slider__item-heading js-heading">Rouge</h3>
+									</div>
+
+									<div class="slider__item-button-wrap">
+										<button
+											class="button slider__item-button js-button"
+											type="button"
+											data-slug="rouge"
+										>
+											<span class="button__title-mask">
+												<div class="button__title-wrap">
+													<span class="button__title">Read more</span>
+													<span class="button__title--secondary"
+														>Read more</span
+													>
+												</div>
+											</span>
+
+											<span class="button__icon">
+												<span class="button__circle"></span>
+												<svg
+													class="button__svg"
+													xmlns="http://www.w3.org/2000/svg"
+													viewBox="0 0 16.002 4.639"
+												>
+													<path d="M.25 2.264h15.403" />
+													<path d="M13.743.354l1.91 1.91-1.919 2.022" />
+												</svg>
+												<svg
+													class="button__svg button__svg--clone"
+													xmlns="http://www.w3.org/2000/svg"
+													viewBox="0 0 16.002 4.639"
+												>
+													<path d="M.25 2.264h15.403" />
+													<path d="M13.743.354l1.91 1.91-1.919 2.022" />
+												</svg>
+											</span>
+										</button>
+									</div>
+								</div>
+							</div>
+
+							<div class="slider__item js-item" style="--aspect-ratio: 0.8">
+								<div
+									class="slider__item-img-wrap js-img-wrap js-img"
+									style="--aspect-ratio: 0.8"
+									data-building="building-slug"
+									data-index="1"
+								>
+									<img
+										src="https://images.unsplash.com/photo-1472835560847-37d024ebacdc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80&ar=0.8"
+										alt="building"
+										class="slider__item-img"
+									/>
+								</div>
+
+								<div class="slider__item-content">
+									<div class="slider__item-heading-wrap">
+										<h3 class="slider__item-heading js-heading">Jaune</h3>
+									</div>
+
+									<div class="slider__item-button-wrap">
+										<button
+											class="button slider__item-button js-button"
+											type="button"
+											data-slug="jaune"
+										>
+											<span class="button__title-mask">
+												<div class="button__title-wrap">
+													<span class="button__title">Read more</span>
+													<span class="button__title--secondary"
+														>Read more</span
+													>
+												</div>
+											</span>
+
+											<span class="button__icon">
+												<span class="button__circle"></span>
+												<svg
+													class="button__svg"
+													xmlns="http://www.w3.org/2000/svg"
+													viewBox="0 0 16.002 4.639"
+												>
+													<path d="M.25 2.264h15.403" />
+													<path d="M13.743.354l1.91 1.91-1.919 2.022" />
+												</svg>
+												<svg
+													class="button__svg button__svg--clone"
+													xmlns="http://www.w3.org/2000/svg"
+													viewBox="0 0 16.002 4.639"
+												>
+													<path d="M.25 2.264h15.403" />
+													<path d="M13.743.354l1.91 1.91-1.919 2.022" />
+												</svg>
+											</span>
+										</button>
+									</div>
+								</div>
+							</div>
+
+							<div class="slider__item js-item" style="--aspect-ratio: 0.8">
+								<div
+									class="slider__item-img-wrap js-img-wrap js-img"
+									style="--aspect-ratio: 0.8"
+									data-building="building-slug"
+									data-index="1"
+								>
+									<img
+										src="https://images.unsplash.com/photo-1541320823636-40247af897bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80&ar=0.8"
+										alt="building"
+										class="slider__item-img"
+									/>
+								</div>
+
+								<div class="slider__item-content">
+									<div class="slider__item-heading-wrap">
+										<h3 class="slider__item-heading js-heading">Orange</h3>
+									</div>
+
+									<div class="slider__item-button-wrap">
+										<button
+											class="button slider__item-button js-button"
+											type="button"
+											data-slug="orange"
+										>
+											<span class="button__title-mask">
+												<div class="button__title-wrap">
+													<span class="button__title">Read more</span>
+													<span class="button__title--secondary"
+														>Read more</span
+													>
+												</div>
+											</span>
+
+											<span class="button__icon">
+												<span class="button__circle"></span>
+												<svg
+													class="button__svg"
+													xmlns="http://www.w3.org/2000/svg"
+													viewBox="0 0 16.002 4.639"
+												>
+													<path d="M.25 2.264h15.403" />
+													<path d="M13.743.354l1.91 1.91-1.919 2.022" />
+												</svg>
+												<svg
+													class="button__svg button__svg--clone"
+													xmlns="http://www.w3.org/2000/svg"
+													viewBox="0 0 16.002 4.639"
+												>
+													<path d="M.25 2.264h15.403" />
+													<path d="M13.743.354l1.91 1.91-1.919 2.022" />
+												</svg>
+											</span>
+										</button>
+									</div>
+								</div>
+							</div>
+
+							<div class="slider__item js-item" style="--aspect-ratio: 0.8">
+								<div
+									class="slider__item-img-wrap js-img-wrap js-img"
+									style="--aspect-ratio: 0.8"
+									data-building="building-slug"
+									data-index="1"
+								>
+									<img
+										src="https://images.unsplash.com/photo-1566787020216-3e4f973ec5ec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80&ar=0.8"
+										alt="building"
+										class="slider__item-img"
+									/>
+								</div>
+
+								<div class="slider__item-content">
+									<div class="slider__item-heading-wrap">
+										<h3 class="slider__item-heading js-heading">Vert</h3>
+									</div>
+
+									<div class="slider__item-button-wrap">
+										<button
+											class="button slider__item-button js-button"
+											type="button"
+											data-slug="vert"
+										>
+											<span class="button__title-mask">
+												<div class="button__title-wrap">
+													<span class="button__title">Read more</span>
+													<span class="button__title--secondary"
+														>Read more</span
+													>
+												</div>
+											</span>
+
+											<span class="button__icon">
+												<span class="button__circle"></span>
+												<svg
+													class="button__svg"
+													xmlns="http://www.w3.org/2000/svg"
+													viewBox="0 0 16.002 4.639"
+												>
+													<path d="M.25 2.264h15.403" />
+													<path d="M13.743.354l1.91 1.91-1.919 2.022" />
+												</svg>
+												<svg
+													class="button__svg button__svg--clone"
+													xmlns="http://www.w3.org/2000/svg"
+													viewBox="0 0 16.002 4.639"
+												>
+													<path d="M.25 2.264h15.403" />
+													<path d="M13.743.354l1.91 1.91-1.919 2.022" />
+												</svg>
+											</span>
+										</button>
+									</div>
+								</div>
+							</div>
+
+							<div class="slider__item js-item" style="--aspect-ratio: 0.8">
+								<div
+									class="slider__item-img-wrap js-img-wrap js-img"
+									style="--aspect-ratio: 0.8"
+									data-building="building-slug"
+									data-index="1"
+								>
+									<img
+										src="https://images.unsplash.com/photo-1523181729404-8a3b7b6f918d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80&ar=0.8"
+										alt="building"
+										class="slider__item-img"
+									/>
+								</div>
+
+								<div class="slider__item-content">
+									<div class="slider__item-heading-wrap">
+										<h3 class="slider__item-heading js-heading">Bleu</h3>
+									</div>
+
+									<div class="slider__item-button-wrap">
+										<button
+											class="button slider__item-button js-button"
+											type="button"
+											data-slug="bleu"
+										>
+											<span class="button__title-mask">
+												<div class="button__title-wrap">
+													<span class="button__title">Read more</span>
+													<span class="button__title--secondary"
+														>Read more</span
+													>
+												</div>
+											</span>
+
+											<span class="button__icon">
+												<span class="button__circle"></span>
+												<svg
+													class="button__svg"
+													xmlns="http://www.w3.org/2000/svg"
+													viewBox="0 0 16.002 4.639"
+												>
+													<path d="M.25 2.264h15.403" />
+													<path d="M13.743.354l1.91 1.91-1.919 2.022" />
+												</svg>
+												<svg
+													class="button__svg button__svg--clone"
+													xmlns="http://www.w3.org/2000/svg"
+													viewBox="0 0 16.002 4.639"
+												>
+													<path d="M.25 2.264h15.403" />
+													<path d="M13.743.354l1.91 1.91-1.919 2.022" />
+												</svg>
+											</span>
+										</button>
+									</div>
+								</div>
+							</div>
+
+							<div class="slider__item js-item" style="--aspect-ratio: 0.8">
+								<div
+									class="slider__item-img-wrap js-img-wrap js-img"
+									style="--aspect-ratio: 0.8"
+									data-building="building-slug"
+									data-index="1"
+								>
+									<img
+										src="https://images.unsplash.com/photo-1566688342604-dbe3e7357104?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80&ar=0.8"
+										alt="building"
+										class="slider__item-img"
+									/>
+								</div>
+
+								<div class="slider__item-content">
+									<div class="slider__item-heading-wrap">
+										<h3 class="slider__item-heading js-heading">Violet</h3>
+									</div>
+
+									<div class="slider__item-button-wrap">
+										<button
+											class="button slider__item-button js-button"
+											type="button"
+											data-slug="violet"
+										>
+											<span class="button__title-mask">
+												<div class="button__title-wrap">
+													<span class="button__title">Read more</span>
+													<span class="button__title--secondary"
+														>Read more</span
+													>
+												</div>
+											</span>
+
+											<span class="button__icon">
+												<span class="button__circle"></span>
+												<svg
+													class="button__svg"
+													xmlns="http://www.w3.org/2000/svg"
+													viewBox="0 0 16.002 4.639"
+												>
+													<path d="M.25 2.264h15.403" />
+													<path d="M13.743.354l1.91 1.91-1.919 2.022" />
+												</svg>
+												<svg
+													class="button__svg button__svg--clone"
+													xmlns="http://www.w3.org/2000/svg"
+													viewBox="0 0 16.002 4.639"
+												>
+													<path d="M.25 2.264h15.403" />
+													<path d="M13.743.354l1.91 1.91-1.919 2.022" />
+												</svg>
+											</span>
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
 				</section>
 
@@ -69,5 +474,46 @@
 </template>
 
 <script>
-export default {};
+import scroll from "virtual-scroll";
+
+export default {
+	methods: {
+		render() {
+			const scrollLast = new scroll().state.last;
+
+			this.items.forEach((item) => {
+				const { bounds } = item;
+				const inView =
+					scrollLast + window.innerWidth >= bounds.left &&
+					scrollLast < bounds.right;
+
+				if (inView) {
+					const min = bounds.left - window.innerWidth;
+					const max = bounds.right;
+					const percentage = ((scrollLast - min) * 100) / (max - min);
+					const newMin = -(window.innerWidth / 12) * 3;
+					const newMax = 0;
+					item.x = ((percentage - 0) / (100 - 0)) * (newMax - newMin) + newMin;
+
+					item.img.style.transform = `translate3d(${item.x}px, 0, 0) scale(1.75)`;
+				}
+			});
+		},
+	},
+	mounted() {
+		this.dom = {};
+		this.dom.el = window.document.querySelector(".js-slider");
+		this.dom.container = this.dom.el.querySelector(".js-container");
+		this.dom.items = this.dom.el.querySelectorAll(".js-item");
+		this.dom.images = this.dom.el.querySelectorAll(".js-img-wrap");
+		this.dom.progress = this.dom.el.querySelector(".js-progress");
+
+		const max = -this.dom.container.offsetWidth + window.innerWidth;
+		const progress = ((scroll.state.last - 0) * 100) / (max - 0) / 100;
+
+		this.dom.progress.style.transform = `scaleX(${progress})`;
+
+		render()
+	},
+};
 </script>
