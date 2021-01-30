78d2386a29550cd855038fad2143165ae588bcfb
Neosoulink <neodarksoulink@gmail.com>
2021-01-30 21:19:02 +0100

    Implementation projectList in vue data and display them

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -7,27 +7,37 @@
 			</div>
 
 			<div class="row">
-				<div class="col-md-6 mb-4" v-for="(item, index) in ['items', 'fd']" :key="index" @load="initPortfolioCard(this)">
+				<div
+					class="col-md-6 mb-4"
+					v-for="(project, index) in projectList"
+					:key="index"
+				>
 					<div class="portfolio-card-wrapper">
 						<div class="portfolio-card">
 							<div class="portfolio-card-front">
 								<div class="image_overlay"></div>
-								<div class="view_details btn-primary-custom">
-									View details
-								</div>
+								<div class="view_details btn-primary-custom">View details</div>
 								<div class="stats-container">
-									<h3 class="project-name">Virtual Class</h3>
+									<h3 class="project-name">{{ project.label }}</h3>
 
 									<div class="project-links">
 										<div class="bar-separator"></div>
 										<ul>
-											<li>
-												<a href="#" target="_blank" title="Live demo">
+											<li v-if="project.liveDemoLink">
+												<a
+													:href="project.liveDemoLink"
+													target="_blank"
+													title="Live demo"
+												>
 													<font-awesome-icon :icon="['fas', 'eye']" />
 												</a>
 											</li>
-											<li>
-												<a href="#" target="_blank" title="Github repo">
+											<li v-if="project.repoLink">
+												<a
+													:href="project.repoLink"
+													target="_blank"
+													title="Github repo"
+												>
 													<font-awesome-icon :icon="['fab', 'github']" />
 												</a>
 											</li>
@@ -41,22 +51,21 @@
 
 							<div class="portfolio-card-back">
 								<div class="description-wrapper">
-									<h3 class="description-title">Virtual Class</h3>
+									<h3 class="description-title">{{ project.label }}</h3>
 
 									<div class="bar-separator"></div>
 									<p class="description-content">
-										Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-										Omnis autem voluptatem in.
+										{{ project.description }}
 									</p>
 								</div>
 
 								<div class="footer">
-									<a href="#" class="mr-2" target="_blank" title="Live demo">
+									<a v-if="project.liveDemoLink" :href="project.liveDemoLink" class="mr-2" target="_blank" title="Live demo">
 										<font-awesome-icon :icon="['fas', 'eye']" />
 										Live demo
 									</a>
 
-									<a href="#" target="_blank" title="Live demo">
+									<a v-if="project.repoLink" :href="project.repoLink" target="_blank" title="Live demo">
 										<font-awesome-icon :icon="['fab', 'github']" />
 										Github repo
 									</a>
@@ -81,11 +90,17 @@
 </template>
 
 <script>
-import animatePortfolioCard from '../assets/js/animatePortfolioCard';
+import animatePortfolioCard from "../assets/js/animatePortfolioCard";
 export default {
 	data() {
 		return {
-			projectList : []
+			projectList: [
+				{
+					label: "Virtual Class",
+					description: "An multi tache application for quick creation app",
+					repoLink: "https://github.com/Neosoulink/VirtualClass",
+				},
+			],
 		};
 	},
 	mounted() {
