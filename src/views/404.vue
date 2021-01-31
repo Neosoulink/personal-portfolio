fabc378aea6a43924ffc4ded8a448ddf0ed3de81
Neosoulink <neodarksoulink@gmail.com>
2021-01-31 15:45:12 +0100

    Fix 404 page dipplay

--- src/views/404.vue
+++ src/views/404.vue
@@ -6,6 +6,10 @@
 			<img src="@/assets/img/undraw_Taken_if77.svg" class="img-404" alt="">
 			<div class="title-block">
 				<h3>404 - Page not found</h3>
+				<router-link :to="{name: 'Home'}">
+					<font-awesome-icon :icon="['fas', 'home']" />
+					Go home
+				</router-link >
 			</div>
 			<!-- /.title-block -->
 		</div>
