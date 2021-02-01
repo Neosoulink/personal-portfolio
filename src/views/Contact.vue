bca48e42bb5956a66eb8c709b0162094dd89b5b8
Neosoulink <neodarksoulink@gmail.com>
2021-02-01 14:37:10 +0100

    Fix some bug and code review

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -11,7 +11,9 @@
 				<div class="lm-info-block gray-default">
 					<font-awesome-icon icon="phone-alt" />
 					<h4>
-						<a href="tel:+243 823 132 630" target="_blank">Call me: +243 823 132 630</a>
+						<a href="tel:+243 823 132 630" target="_blank"
+							>Call me: +243 823 132 630</a
+						>
 					</h4>
 				</div>
 
@@ -21,7 +23,8 @@
 						<a
 							href="https://wa.me/+243823132630?text=I'm%20interested%20about%20your%20profile"
 							target="_blank"
-						>Contact me in Whatsapp</a>
+							>Contact me in Whatsapp</a
+						>
 					</h4>
 				</div>
 
@@ -31,45 +34,57 @@
 						<a
 							href="https://www.linkedin.com/in/nathan-mandemvo-87b0b2196/"
 							target="_blank"
-						>Join me in Linkedin</a>
+							>Join me in Linkedin</a
+						>
 					</h4>
 				</div>
 
 				<div class="lm-info-block gray-default">
 					<font-awesome-icon :icon="['fab', 'twitter']" />
 					<h4>
-						<a href="https://twitter.com/nsl_nathan" target="_blank">Follow me on Twitter</a>
+						<a href="https://twitter.com/nsl_nathan" target="_blank"
+							>Follow me on Twitter</a
+						>
 					</h4>
 				</div>
 
 				<div class="lm-info-block gray-default">
 					<font-awesome-icon :icon="['fab', 'facebook']" />
 					<h4>
-						<a href="https://web.facebook.com/NSL.Solaris" target="_blank">Be freind on Facebook</a>
+						<a href="https://web.facebook.com/NSL.Solaris" target="_blank"
+							>Be freind on Facebook</a
+						>
 					</h4>
 				</div>
 
 				<div class="lm-info-block gray-default">
 					<font-awesome-icon :icon="['fab', 'github']" />
 					<h4>
-						<a href="https://github.com/Neosoulink" target="_blank">Follow me on Github</a>
+						<a href="https://github.com/Neosoulink" target="_blank"
+							>Follow me on Github</a
+						>
 					</h4>
 				</div>
 
 				<div class="lm-info-block gray-default">
 					<font-awesome-icon :icon="['fab', 'instagram']" />
 					<h4>
-						<a href="https://www.instagram.com/destinyofnath/" target="_blank">Follow me on Instagram</a>
+						<a href="https://www.instagram.com/destinyofnath/" target="_blank"
+							>Follow me on Instagram</a
+						>
 					</h4>
 				</div>
 
 				<div class="lm-info-block gray-default">
 					<font-awesome-icon icon="laptop-code" />
 					<h4>
-						<a href="https://www.codingame.com/profile/cc01af57cefe78a2e220ceeb2cc838a09672843" target="_blank">challenge me on codingame</a>
+						<a
+							href="https://www.codingame.com/profile/cc01af57cefe78a2e220ceeb2cc838a09672843"
+							target="_blank"
+							>challenge me on codingame</a
+						>
 					</h4>
 				</div>
-
 
 				<div class="lm-info-block gray-default">
 					<font-awesome-icon icon="at" />
@@ -77,14 +92,19 @@
 						<a
 							href="mailto:nathaninbusines2.0@gmail.com?subject=I'm%20interested%20about%20your%20profile"
 							target="_blank"
-						>nathaninbusines2.0@gmail.com</a>
+							>nathaninbusines2.0@gmail.com</a
+						>
 					</h4>
 				</div>
 
 				<div class="lm-info-block gray-default">
 					<font-awesome-icon :icon="['fab', 'stack-overflow']" class />
 					<h4>
-						<a href="https://stackoverflow.com/users/11686855/neosoulink" target="_blank">stackoverflow</a>
+						<a
+							href="https://stackoverflow.com/users/11686855/neosoulink"
+							target="_blank"
+							>stackoverflow</a
+						>
 					</h4>
 				</div>
 			</div>
@@ -99,47 +119,20 @@
 					<h3>
 						How Can I
 						<span>Help You?</span>
-						<small class="text-danger">(	<font-awesome-icon icon="exclamation-triangle" class /> Actualy not available)</small>
 					</h3>
 				</div>
 
-				<form id="contact_form" class="contact-form" @submit.prevent>
+				<form id="contact_form" @submit.prevent="sendEmail()">
 					<div class="messages"></div>
 
 					<div class="controls two-columns">
 						<div class="fields clearfix">
-							<div class="left-column">
-								<div class="form-group form-group-with-icon" :class="{'form-group-focus' : form.fullName}">
-									<input
-										id="form_name"
-										type="text"
-										name="name"
-										class="form-control-custom"
-										v-model="form.fullName"
-										required="required"
-										data-error="Name is required."
-									/>
-									<label>Full Name</label>
-									<div class="form-control-custom-border"></div>
-									<div class="help-block with-errors"></div>
-								</div>
+							<div class="">
 
-								<div class="form-group form-group-with-icon" :class="{'form-group-focus' : form.email}">
-									<input
-										id="form_email"
-										type="email"
-										name="email"
-										class="form-control-custom"
-										v-model="form.email"
-										required="required"
-										data-error="Valid email is required."
-									/>
-									<label>Email Address</label>
-									<div class="form-control-custom-border"></div>
-									<div class="help-block with-errors"></div>
-								</div>
-
-								<div class="form-group form-group-with-icon" :class="{'form-group-focus' : form.subject}">
+								<div
+									class="form-group form-group-with-icon"
+									:class="{ 'form-group-focus': form.subject }"
+								>
 									<input
 										id="form_subject"
 										type="text"
@@ -153,14 +146,16 @@
 									<div class="form-control-custom-border"></div>
 									<div class="help-block with-errors"></div>
 								</div>
-							</div>
-							<div class="right-column">
-								<div class="form-group form-group-with-icon" :class="{'form-group-focus' : form.message}">
+
+								<div
+									class="form-group form-group-with-icon"
+									:class="{ 'form-group-focus': form.message }"
+								>
 									<textarea
 										id="form_message"
-										name="message"
+										name="body"
 										class="form-control-custom"
-										v-model="form.message"
+										v-model="form.body"
 										rows="7"
 										required="required"
 										data-error="Please, leave me a message."
@@ -172,11 +167,17 @@
 							</div>
 						</div>
 
-						<div
-							class="g-recaptcha"
-							data-sitekey="6LdqmCAUAAAAAMMNEZvn6g4W5e0or2sZmAVpxVqI"
-							data-theme="dark"
-						></div>
+						<div class="g-recaptcha">
+						<vue-recaptcha
+								theme="dark"
+								sitekey="6Leip0QaAAAAABA2pFDnWpUrAbdiEQETMtNwvh15"
+								:loadRecaptchaScript="true"
+								@verify="form.isValidatedRecaptcha = true"
+								@expired="form.isValidatedRecaptcha = false"
+							>
+							</vue-recaptcha>
+							<small v-if="!form.isValidatedRecaptcha" class="text-danger">Please validate the reCAPTCHA</small>
+						</div>
 
 						<input type="submit" class="button btn-send" value="Send message" />
 					</div>
@@ -190,17 +191,33 @@
 </template>
 
 <script>
+import VueRecaptcha from "vue-recaptcha";
+
 export default {
 	name: "Contact",
+	components: { VueRecaptcha },
 	data() {
 		return {
 			form: {
 				fullName: null,
 				email: null,
 				subject: null,
-				message: null
-			}
+				body: null,
+				isValidatedRecaptcha : false
+			},
 		};
-	}
+	},
+	methods: {
+		sendEmail() {
+			if(this.form.isValidatedRecaptcha){
+				window.open(`mailto:nathaninbusiness2.0@gmail.com?subject=${this.form.subject}&body=${this.form.body}`);
+			}else{
+				window.alert('Please, be sure to validate reCaptcha')
+			}
+		}
+	},
+	mounted() {
+		//this.this.$recaptchaInstance.showBadge()
+	},
 };
 </script>
