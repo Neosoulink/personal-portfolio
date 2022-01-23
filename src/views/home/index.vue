eefba6287372d7ab755bb669f613410aefe44a72
Neosoulink <neodarksoulink@gmail.com>
2022-01-23 16:55:18 +0100

    feat: import sections

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,22 @@
 <template>
   <LandingView />
+
+  <div class="w-full min-h-full">
+    <HomeSection />
+    <AboutSection />
+    <ContactSection />
+  </div>
 </template>
 
 <script lang="ts">
+// COMPONENTS
 import LandingView from "../../components/LandingView.vue";
+import HomeSection from "./+home.vue";
+import AboutSection from "./+about.vue";
+import ContactSection from "./+contact.vue";
+
 export default {
   name: "root",
-  components: { LandingView },
+  components: { LandingView, HomeSection, AboutSection, ContactSection },
 };
 </script>
