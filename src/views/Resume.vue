db65eaec2fd0463f73a5852292af57c4873b0e48
Neosoulink <neodarksoulink@gmail.com>
2022-12-31 21:57:06 +0200

    refactor: upgrade resume

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -7,7 +7,7 @@
 
     <div class="section-content">
       <div class="row">
-        <div class="col-12 col-sm-7">
+        <div class="col-12 col-sm-7 mb-4">
           <div class="block-title">
             <h3>Experience</h3>
           </div>
@@ -15,23 +15,68 @@
           <div class="timeline timeline-second-style clearfix">
             <div class="timeline-item clearfix">
               <div class="left-part">
-                <h5 class="item-period">2019 - Current</h5>
-                <span class="item-company">African Law Repport</span>
+                <h5 class="item-period">2022 - Current</h5>
+                <a href="https://ever.tech/" class="item-company">Ever Technologies<font-awesome-icon
+                    icon="external-link-alt" /></a>
               </div>
               <div class="divider"></div>
               <div class="right-part">
-                <h4 class="item-title">Full-Stack Developper</h4>
-                <p>
-                  Creation of a management platform for lawyers. Maintenance of the
-                  official website of the cabinet of Emery Mukendi W. (cabemery.org).
-                  Update of the juriDocs.org software (web and mobile) and Cabemery.org
-                  website.
+                <h4 class="item-title">Full-stack software engineer</h4>
+                <p class="">
+                  I develop existing full-stack applications, I lead teams and guide new developers. It's a real
+                  pleasure to contribute to
+                  Ever...
                 </p>
-                <p>
-                  In direct partnership with
-                  <a href="https://www.cabemery.org/" target="_blank">
-                    www.cabemry.org
-                    <font-awesome-icon icon="external-link-alt" /> </a>.
+              </div>
+            </div>
+
+            <div class="timeline-item clearfix">
+              <div class="left-part">
+                <h5 class="item-period">2021 - Current</h5>
+                <a href="https://tech-congo.com/" target="_blank" class="item-company">Tech Congo <font-awesome-icon
+                    icon="external-link-alt" /></a>
+              </div>
+              <div class="divider"></div>
+              <div class="right-part">
+                <h4 class="item-title">Lead react engineer</h4>
+                <p class="">
+                  Subcontracting for the Keeano company.
+                  Creation of <a href="https://keeano.com" target="_blank">Keeano web <font-awesome-icon
+                      icon="external-link-alt" /></a> and mobile app (Android and
+                  IOS).
+                  Development of IT solutions for various applications
+                </p>
+              </div>
+            </div>
+
+            <div class="timeline-item clearfix">
+              <div class="left-part">
+                <h5 class="item-period">2021</h5>
+                <a href="https://www.maishapay.net" target="_blank" class="item-company">MaishaPay Fintech
+                  <font-awesome-icon icon="external-link-alt" /></a>
+              </div>
+              <div class="divider"></div>
+              <div class="right-part">
+                <h4 class="item-title">Contactor Mobile engineer</h4>
+                <p class="">
+                  Design and development of the new version of the Maishapay mobile app
+                </p>
+              </div>
+            </div>
+
+            <div class="timeline-item clearfix">
+              <div class="left-part">
+                <h5 class="item-period">2019 - 2020</h5>
+                <span class="item-company">African Law Report</span>
+              </div>
+              <div class="divider"></div>
+              <div class="right-part">
+                <h4 class="item-title">Contactor Web & Mobile engineer</h4>
+                <p class="">
+                  Creation of a management platform for lawyers. Maintenance of the website of the company
+                  (<a href="cabemery.org" target="_blank">Cabemery <font-awesome-icon icon="external-link-alt" /></a>).
+                  Maintenance of <a href="juriDocs.org" target="_blank">juriDocs <font-awesome-icon
+                      icon="external-link-alt" /></a> web and mobile apps.
                 </p>
               </div>
             </div>
@@ -43,7 +88,7 @@
               </div>
               <div class="divider"></div>
               <div class="right-part">
-                <h4 class="item-title">Consultant Programmer Engineer</h4>
+                <h4 class="item-title">Contactor web engineer</h4>
                 <p></p>
               </div>
             </div>
@@ -51,35 +96,23 @@
 
           <div class="white-space-50"></div>
 
+          <!-- Knowledges -->
           <div class="block-title">
-            <h3>Education</h3>
+            <h3>Knowledge & Others</h3>
           </div>
 
-          <div class="timeline timeline-second-style clearfix">
-            <div class="timeline-item clearfix">
-              <div class="left-part">
-                <h5 class="item-period">2020 - Current</h5>
-                <span class="item-company">ISC</span>
-              </div>
-              <div class="divider"></div>
-              <div class="right-part">
-                <h4 class="item-title">Licence study</h4>
-                <p></p>
-              </div>
-            </div>
-
-            <div class="timeline-item clearfix">
-              <div class="left-part">
-                <h5 class="item-period">2019</h5>
-                <span class="item-company">Institus superieur de commerce</span>
-              </div>
-              <div class="divider"></div>
-              <div class="right-part">
-                <h4 class="item-title">Informatic graduate</h4>
-                <p>End of my graduate study (graduation).</p>
-              </div>
-            </div>
-          </div>
+          <ul class="knowledges">
+            <li>Figma UI/UX</li>
+            <li>Digital Marketing</li>
+            <li>Project managment with Trello, Jira & CLickUp</li>
+            <li>SEO</li>
+            <li>Web3D</li>
+            <li>System Conception</li>
+            <li>Team player</li>
+            <li>Web3</li>
+            <li>Code testing</li>
+          </ul>
+          <!-- End of Knowledges -->
         </div>
 
         <!-- Skills & Certificates -->
@@ -102,108 +135,21 @@
 
           <div class="block-title">
             <h3>
-              Database
+              DBMS / Services
               <span>Used</span>
             </h3>
           </div>
 
           <div class="skills-info skills-second-style">
-            <Skill title="MySQL" :percentage="75"></Skill>
-            <Skill title="Oracle" :percentage="65"></Skill>
             <Skill title="Firebase" :percentage="70"></Skill>
-            <Skill title="MongoDB" :percentage="75"></Skill>
+            <Skill title="MySQL" :percentage="75"></Skill>
+            <Skill title="MongoDB" :percentage="65"></Skill>
           </div>
           <!-- End of Coding Skills -->
-
-          <div class="white-space-10"></div>
-
-          <!-- Knowledges -->
-          <div class="block-title">
-            <h3>Knowledges</h3>
-          </div>
-
-          <ul class="knowledges">
-            <li>Marketing Digital</li>
-            <li>Print Software</li>
-            <li>SEO</li>
-            <li>System Conception</li>
-          </ul>
-          <!-- End of Knowledges -->
-        </div>
-        <!-- End of Skills & Certificates -->
-      </div>
-
-      <div class="white-space-50"></div>
-
-      <!-- Certificates -->
-      <div class="row">
-        <div class="col-12 col-sm-12">
-          <div class="block-title">
-            <h3>Certificates</h3>
-          </div>
         </div>
       </div>
-
-      <div class="row">
-        <!-- Certificate 1 -->
-        <div class="col-12 col-sm-6">
-          <div class="certificate-item clearfix">
-            <div class="certi-logo">
-              <img src="https://skillvalue.com/assets/img/logo.pentalog-company-white.svg" alt="logo" />
-            </div>
-
-            <div class="certi-content">
-              <div class="certi-title">
-                <h4>
-                  <a href="https://skillvalue.com/fr/user/certificate/1IIwT7FONwBlJCrOjaL3piDfp8LAzUaJJfmubKhJPPw1rJoBODiAGGAbaC2e"
-                    target="_blank">Vuejs Certificate</a>
-                </h4>
-              </div>
-            </div>
-          </div>
-        </div>
-        <!-- End of Certificate 1 -->
-
-        <!-- Certificate 2 -->
-        <div class="col-12 col-sm-6">
-          <div class="certificate-item clearfix">
-            <div class="certi-logo">
-              <img src="https://skillvalue.com/assets/img/logo.pentalog-company-white.svg" alt="logo" />
-            </div>
-
-            <div class="certi-content">
-              <div class="certi-title">
-                <h4>
-                  <a href="https://skillvalue.com/fr/user/certificate/6xgAFXiInVW8HNONdMe8J84I7Zc0SV07MUdwokborxf2C4tEJkCXV5pmWPRr"
-                    target="_blank">Laravel Certificate</a>
-                </h4>
-              </div>
-            </div>
-          </div>
-        </div>
-        <!-- End of Certificate 2 -->
-
-        <!-- Certificate 2 -->
-        <div class="col-12 col-sm-6">
-          <div class="certificate-item clearfix">
-            <div class="certi-logo">
-              <img src="https://skillvalue.com/assets/img/logo.pentalog-company-white.svg" alt="logo" />
-            </div>
-
-            <div class="certi-content">
-              <div class="certi-title">
-                <h4>
-                  <a href="https://skillvalue.com/fr/user/certificate/uMKjt1g4GJgYuIiq4tRtnW60PsuIVL7w94dyfd39yoBpMcvC6SBqn8CkpXKg"
-                    target="_blank">JavaScript ES6 Certificate</a>
-                </h4>
-              </div>
-            </div>
-          </div>
-        </div>
-        <!-- End of Certificate 2 -->
-      </div>
-      <!-- End of Certificates -->
     </div>
+
   </section>
   <!-- End of Resume Subpage -->
 </template>
