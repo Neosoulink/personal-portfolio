766414d80a3f9e043efe8313dda0f6dd46eddce3
Neosoulink <neodarksoulink@gmail.com>
2023-02-04 21:37:06 +0200

    feat: add header layout

---
+++ layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+	<LayoutHeader />
+
+	<slot />
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
 	<LandingView />
 
-	<main class="h-screen w-full bg-dark-radial-gradient"></main>
+	<main class=" min-h-screen w-full bg-dark-radial-gradient"></main>
 </template>
