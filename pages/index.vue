0284871e9e6cbc0c6013f39ccfdcac5fc5a4dedc
Neosoulink <neodarksoulink@gmail.com>
2023-02-04 23:28:23 +0200

    refactor: minor

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,8 @@
 		<LandingView />
 
 		<main class="min-h-screen w-full bg-dark-radial-gradient text-white">
-			<section class="h-screen flex items-center pl-14">
-				<h1 class="text-glitch text-9xl relative" data-text="I MAKE THINKS">I MAKE THINKS</h1>
+			<section class="h-screen flex items-center pl-28">
+				<h1 class="text-glitch text-9xl font-semibold relative" data-text="I MAKE THINKS">I MAKE THINKS</h1>
 			</section>
 		</main>
 	</div>
