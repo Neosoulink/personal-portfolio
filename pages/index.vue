6bb1aa850e4c43cb448cb475465a6c8959bd66ac
Neosoulink <neodarksoulink@gmail.com>
2023-02-05 12:47:34 +0200

    refactor: handle first title hover

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,35 @@
 	<div>
 		<LandingView />
 
-		<main class="min-h-screen w-full bg-dark-radial-gradient text-white">
+		<main class="w-full bg-dark-radial-gradient text-white">
 			<section class="h-screen flex items-center pl-28">
-				<h1 class="text-glitch text-9xl font-semibold relative" data-text="I MAKE THINKS">I MAKE THINKS</h1>
+				<h1 ref="firstTitle" class="text-glitch text-9xl font-semibold relative" :data-text="firstTitle">{{
+					firstTitle
+				}}</h1>
 			</section>
 		</main>
 	</div>
 </template>
+
+<script lang="ts">
+export default {
+	data() {
+		return {
+			firstTitle: "I MAKE THINKS",
+		};
+	},
+	mounted() {
+		(this.$refs.firstTitle as HTMLHeadingElement).addEventListener("mouseover", () => {
+			this.firstTitle = "I BREAK THINKS"
+		});
+		(this.$refs.firstTitle as HTMLHeadingElement).addEventListener("mouseout", () => {
+			this.firstTitle = "I MAKE THINKS"
+		});
+	},
+	beforeUnmount() {
+		(this.$refs.firstTitle as HTMLHeadingElement).removeEventListener("mouseover", () => { });
+		(this.$refs.firstTitle as HTMLHeadingElement).removeEventListener("mouseout", () => { });
+
+	}
+}
+</script>
