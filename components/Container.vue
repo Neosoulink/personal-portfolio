3f10c78dd169f8dd242e88964434ac2626d9e144
Neosoulink <neodarksoulink@gmail.com>
2023-03-31 14:28:18 +0300

    feat: adds custom container component

---
+++ components/Container.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="container mx-auto md:px-20 px-2" :class="className" :style="style">
+		<slot />
+	</div>
+</template>
+
+<script lang="ts">
+export default {
+	props: {
+		style: Object,
+		className: String,
+	},
+};
+</script>
+q
