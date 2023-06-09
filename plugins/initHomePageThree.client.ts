409fe0965c86dc7c7c860a179333b4693e48c791
Neosoulink <neodarksoulink@gmail.com>
2023-06-09 15:24:16 +0300

    refactor: implment correct models + baking

--- plugins/initHomePageThree.client.ts
+++ plugins/initHomePageThree.client.ts
@@ -64,6 +64,8 @@ export const initHomePageThree = () => {
 	const GLTF_LOADER = new GLTFLoader(LOADING_MANAGER);
 	GLTF_LOADER.setDRACOLoader(DRACO_LOADER);
 
+	const TEXTURE_LOADER = new THREE.TextureLoader(LOADING_MANAGER);
+
 	// CLOCK
 	const ANIMATION_CLOCK = new THREE.Clock();
 
@@ -71,45 +73,47 @@ export const initHomePageThree = () => {
 	const APP_GROUP = new THREE.Group();
 
 	// LIGHTS
-	const DIRECTIONAL_LIGHT = new THREE.DirectionalLight(0xffffff, 0.2);
-	DIRECTIONAL_LIGHT.position.set(0, 0, 1);
+	const AMBIENT_LIGHT = new THREE.AmbientLight(0xffffff, 0);
 
-	const AMBIENT_LIGHT = new THREE.AmbientLight(0xffffff, 0.2);
+	/**
+	 * Texture
+	 */
+	const BAKED_TEXTURE = TEXTURE_LOADER.load(
+		"/3d_models/isometric_room/baked-isometric-room.jpg"
+	);
+	BAKED_TEXTURE.flipY = false;
+	BAKED_TEXTURE.colorSpace = THREE.SRGBColorSpace;
+
+	/**
+	 * Materials
+	 */
+	const BAKED_MATERIAL = new THREE.MeshBasicMaterial({
+		map: BAKED_TEXTURE,
+		side: THREE.DoubleSide,
+	});
 
 	// MODELS
 	GLTF_LOADER.load("/3d_models/isometric_room/isometric_room.glb", (glb) => {
-		console.log("Isometric room", glb);
+		const _REG = new RegExp(/.*screen/);
 
-		const OBJ_POS = {
-			x: glb.scene.position.x,
-			y: glb.scene.position.y,
-			z: glb.scene.position.z,
-		};
-
-		const RECT_AREA_TOP_LIGHT = new THREE.RectAreaLight(0xffffff, 6, 1.5, 1.5);
-		RECT_AREA_TOP_LIGHT.position.set(
-			OBJ_POS.x - 1.5,
-			OBJ_POS.y + 14,
-			OBJ_POS.z - 12
-		);
-		RECT_AREA_TOP_LIGHT.lookAt(
-			new THREE.Vector3(OBJ_POS.x - 1.5, OBJ_POS.y, OBJ_POS.z - 12)
-		);
-
-		glb.scene.add(RECT_AREA_TOP_LIGHT);
+		glb.scene.position.y = -2;
+		glb.scene.traverse((child) => {
+			if (child instanceof THREE.Mesh && !_REG.test(child.name)) {
+				child.material = BAKED_MATERIAL;
+			}
+		});
 
 		APP.scene.add(glb.scene);
 	});
 
 	// CAMERA
-	APP.camera.position.setZ(15);
+	APP.camera.position.set(4, 2, 20);
 
 	// ADD TO SCENE
-	APP_GROUP.add(DIRECTIONAL_LIGHT, AMBIENT_LIGHT);
+	APP_GROUP.add(AMBIENT_LIGHT);
 	APP.scene.add(APP_GROUP);
 
 	// ANIMATIONS
-
 	APP.setUpdateCallback("root", () => {
 		const ELAPSED_TIME = ANIMATION_CLOCK.getElapsedTime();
 		const DELTA_TIME = ELAPSED_TIME - previewsElapseTime;
