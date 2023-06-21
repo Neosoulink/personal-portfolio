1a76229e325ff53e6d572eeb8f6b163070a99855
Neosoulink <neodarksoulink@gmail.com>
2023-06-21 14:44:13 +0300

    feat: implement cuved camera path

--- plugins/Experience.client.ts
+++ plugins/Experience.client.ts
@@ -4,7 +4,6 @@ import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import GUI from "lil-gui";
 import GSAP from "gsap";
-import Camera from "quick-threejs/lib/Camera";
 
 export interface ExperienceProps {
 	/**
@@ -27,20 +26,38 @@ export class Experience {
 	gui?: GUI;
 	loadingManager = new THREE.LoadingManager();
 	isometricRoom?: THREE.Group;
+	cameraCurvePathProgress = 0;
+	curvePosition = new THREE.Vector3(0, -1, 0);
+	curveLookAtPosition = new THREE.Vector3(0, 0, 0);
+
+	directionalVector = new THREE.Vector3(0, 0, 0);
+	staticVector = new THREE.Vector3(0, 1, 0);
+	crossVector = new THREE.Vector3(0, 0, 0);
+	started = false;
+	lerp = {
+		current: 0,
+		target: 0,
+		ease: 0.1,
+	};
 	onConstruct?: () => unknown;
 	onDestruct?: () => unknown;
+	back = false;
 
 	constructor(props: ExperienceProps) {
 		this.app = new QuickThree(
 			{
 				enableControls: true,
 				axesSizes: 5,
-				gridSizes: 10,
+				gridSizes: 20,
 				withMiniCamera: true,
 				camera: "Perspective",
 			},
 			props.domElementRef
 		);
+		if (this.app._camera.controls) {
+			this.app._camera.controls.enabled = false;
+		}
+
 		this.gui = this.app.debug?.ui?.addFolder(Experience.name);
 		this.gui?.add({ fn: () => this.construct() }, "fn").name("Enable");
 		this.gui?.close();
@@ -142,13 +159,14 @@ export class Experience {
 				(glb) => {
 					const _REG = new RegExp(/.*screen/);
 
+					glb.scene.scale.set(0.5, 0.5, 0.5);
 					glb.scene.traverse((child) => {
 						if (child instanceof THREE.Mesh && !_REG.test(child.name)) {
 							child.material = BAKED_MATERIAL;
 						}
 					});
 
-					(this.isometricRoom = glb.scene).position.set(0, -10, -15);
+					(this.isometricRoom = glb.scene).position.set(0, -5, -10);
 					(this.isometricRoom = glb.scene).rotation.set(0.3, -0.2, 0);
 
 					this.mainGroup && this.mainGroup.add(this.isometricRoom);
@@ -159,15 +177,108 @@ export class Experience {
 			// @ts-ignore Proxy class error
 			if (this.app.camera?.fov) this.app.camera.fov = 35;
 			this.app.camera?.updateProjectionMatrix();
-			this.app.camera?.position.set(0, 4, 20);
+			this.app.camera?.position.set(0, 0, 20);
+			this.app._camera.miniCamera?.position.set(10, 8, 30);
+			if (this.app.camera?.far) {
+				this.app.camera.far = 50;
+			}
+
+			// HELPERS
+			const CAMERA_HELPER = this.app.camera
+				? new THREE.CameraHelper(this.app.camera)
+				: undefined;
 
 			// ADD TO SCENE
 			this.mainGroup.add(AMBIENT_LIGHT, DIRECTIONAL_LIGHT);
+			CAMERA_HELPER && this.mainGroup.add(CAMERA_HELPER);
 			this.app.scene.add(this.mainGroup);
 
+			// Create a closed wavey loop
+			const CURVE = new THREE.CatmullRomCurve3([
+				new THREE.Vector3(0, 2.5, 10),
+				new THREE.Vector3(7.25, 4, 7.25),
+				new THREE.Vector3(10, 2.5, 0),
+			]);
+
+			const POINTS = CURVE.getPoints(50);
+			const GEOMETRY = new THREE.BufferGeometry().setFromPoints(POINTS);
+
+			const MATERIAL = new THREE.LineBasicMaterial({ color: 0xff0000 });
+
+			// Create the final object to add to the scene
+			const CURVE_OBJECT = new THREE.Line(GEOMETRY, MATERIAL);
+
+			this.app.scene.add(CURVE_OBJECT);
+			this.onWheel();
+
 			// ANIMATIONS
-			this.app.setUpdateCallback("root", () => this.animate());
+			this.app.setUpdateCallback("root", () => {
+				if (CAMERA_HELPER) {
+					CAMERA_HELPER.matrixWorldNeedsUpdate = true;
+					CAMERA_HELPER.update();
+					this.app.camera?.position &&
+						CAMERA_HELPER.position.copy(this.app.camera?.position);
+					this.app.camera?.rotation &&
+						CAMERA_HELPER.rotation.copy(this.app.camera?.rotation);
+				}
+
+				if (this.started) {
+					this.lerp.current = GSAP.utils.interpolate(
+						this.lerp.current,
+						this.lerp.target,
+						this.lerp.ease
+					);
+
+					this.lerp.target = this.lerp.target + (this.back ? -0.001 : 0.001);
+					this.lerp.target = GSAP.utils.clamp(0, 1, this.lerp.target);
+					this.lerp.current = GSAP.utils.clamp(0, 1, this.lerp.current);
+
+					CURVE.getPointAt(this.lerp.current % 1, this.curvePosition);
+					CURVE.getPointAt(
+						GSAP.utils.clamp(
+							0,
+							1,
+							(this.lerp.current + (this.back ? -0.00001 : 0.00001)) % 1
+						),
+						this.curveLookAtPosition
+					);
+
+					this.directionalVector.subVectors(
+						CURVE.getPointAt(
+							GSAP.utils.clamp(0, 1, (this.lerp.current % 1) + 0.000001)
+						),
+						this.curvePosition
+					);
+					this.directionalVector.normalize();
+					this.crossVector.crossVectors(
+						this.directionalVector,
+						this.staticVector
+					);
+					this.crossVector.multiplyScalar(100000);
+
+					this.app.camera?.position.copy(this.curvePosition);
+					if (this.isometricRoom) {
+						const P = new THREE.Vector3().copy(this.isometricRoom.position);
+						P.y += 1;
+						this.app.camera?.lookAt(P);
+						if (this.app._camera.controls)
+							this.app._camera.controls.target = P;
+					}
+				}
+			});
 		}
+	}
+
+	onWheel() {
+		window.addEventListener("wheel", (e) => {
+			if (e.deltaY < 0) {
+				this.lerp.target += 0.1;
+				this.back = false;
+			} else {
+				this.lerp.target -= 0.1;
+				this.back = true;
+			}
+		});
 	}
 
 	start() {
@@ -175,6 +286,7 @@ export class Experience {
 			duration: 2.5,
 			ease: "M0,0 C0.001,0.001 0.002,0.003 0.003,0.004 0.142,0.482 0.284,0.75 0.338,0.836 0.388,0.924 0.504,1 1,1 ",
 		};
+
 		if (this.mainGroup && this.isometricRoom) {
 			GSAP.to(this.isometricRoom.rotation, {
 				x: 0,
@@ -182,16 +294,15 @@ export class Experience {
 				..._DEFAULT_PROPS,
 			});
 			GSAP.to(this.isometricRoom.position, {
-				y: -2.5,
+				y: 0,
 				z: 0,
 				..._DEFAULT_PROPS,
 			});
 		}
-	}
 
-	animate() {
-		if (this.mainGroup?.position) {
-			// this.app.camera.lookAt(new THREE.Vector3(0, 0, 0));
+		this.started = true;
+		if (this.lerp.target < 0) {
+			this.lerp.target = 1;
 		}
 	}
 }
