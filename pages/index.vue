0275cc5eb72313b44dab8975ea8c367021e107cf
Neosoulink <neodarksoulink@gmail.com>
2023-08-14 09:58:07 +0300

    refactor: implement first section home page

--- pages/index.vue
+++ pages/index.vue
@@ -38,7 +38,27 @@ onBeforeUnmount(() => {
 
 		<canvas :id="STATES.domElementID" class="fixed top-0 left-0 -z-10" />
 
-		<div class="flex-1"></div>
+		<Container class="flex-1 flex items-center text-light">
+			<section class="flex-1 flex">
+				<h2 class="text-7xl w-1/2 font-bold leading-normal">
+					WELCOME TO MY SPACE SHIP
+				</h2>
+			</section>
+
+			<div
+				class="relative min-h-[33.33%] flex flex-col justify-center items-center"
+			>
+				<div class="absolute w-1 h-full rounded-sm bg-light" />
+
+				<div class="absolute h-full flex flex-col justify-center items-center">
+					<div
+						v-for="i in Array(3).keys()"
+						:key="i"
+						class="rounded-full h-4 w-4 cursor-pointer bg-light my-2 transition-all hover:h-6 hover:w-6 hover:my-1"
+					/>
+				</div>
+			</div>
+		</Container>
 
 		<div id="mode-bubbles-container" />
 	</main>
