e9cd965331b5b6882ad7e7799b15ef66da135df7
MNAth_ <neodarksoulink@gmail.com>
2023-11-26 19:13:29 +0300

    refactor: removed unused code logic

--- src/interfaces/experienceEvents.ts
+++
@@ -1,9 +0,0 @@
-import { EventEmitter } from "events";
-import type { ExperienceBase } from "./experienceBase";
-/**
- * Represent All supported events of an experience class
- */
-export interface ExperienceEvents extends ExperienceBase, EventEmitter {
-	update?: () => unknown;
-	resize?: () => unknown;
-}
