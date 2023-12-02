2f00dec9949786a54126d2b1c49b8b820fbbdeac
MNAth_ <neodarksoulink@gmail.com>
2023-12-02 10:43:46 +0300

    feat: implement renderer `localClippingEnabled`

--- src/experiences/pages/Home/Renderer.ts
+++ src/experiences/pages/Home/Renderer.ts
@@ -9,24 +9,26 @@ import { type ExperienceBase } from "@/interfaces/experienceBase";
 /** Renderer */
 export default class Renderer implements ExperienceBase {
 	protected readonly _experience = new HomeExperience();
+	protected readonly _appRenderer = this._experience.app.renderer;
 
 	constructor() {}
 
 	construct() {
-		this._experience.app.renderer.instance.useLegacyLights = true;
-		this._experience.app.renderer.instance.outputColorSpace = SRGBColorSpace;
-		this._experience.app.renderer.instance.toneMapping = ACESFilmicToneMapping;
-		this._experience.app.renderer.instance.toneMappingExposure = 1;
-		this._experience.app.renderer.instance.shadowMap.enabled = false;
-		this._experience.app.renderer.instance.shadowMap.type = PCFShadowMap;
-		this._experience.app.renderer.instance.setClearColor("#211d20");
-		this._experience.app.renderer.instance.setSize(
+		this._appRenderer.instance.useLegacyLights = true;
+		this._appRenderer.instance.outputColorSpace = SRGBColorSpace;
+		this._appRenderer.instance.toneMapping = ACESFilmicToneMapping;
+		this._appRenderer.instance.toneMappingExposure = 1;
+		this._appRenderer.instance.shadowMap.enabled = false;
+		this._appRenderer.instance.shadowMap.type = PCFShadowMap;
+		this._appRenderer.instance.setClearColor("#211d20");
+		this._appRenderer.instance.setSize(
 			this._experience.app.sizes.width,
 			this._experience.app.sizes.height
 		);
-		this._experience.app.renderer.instance.setPixelRatio(
+		this._appRenderer.instance.setPixelRatio(
 			this._experience.app.sizes.pixelRatio
 		);
+		this._appRenderer.instance.localClippingEnabled = true
 	}
 
 	destruct() {}
