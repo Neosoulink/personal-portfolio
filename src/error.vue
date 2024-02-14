8e30920b6eea377ae11d8f459a9087c85c6d9221
MNAth_ <neodarksoulink@gmail.com>
2024-02-14 22:01:23 +0300

    feat: design error page

--- src/error.vue
+++ src/error.vue
@@ -5,8 +5,15 @@ const props = defineProps({
 </script>
 
 <template>
-	<div>
-		<h1>{{ props.error?.statusCode }}</h1>
-		<NuxtLink to="/">Go back home</NuxtLink>
+	<div
+		class="flex flex-col items-center justify-center text-center bg-dark text-light h-dvh w-dvw"
+	>
+		<h1 class="mb-1 font-medium opacity-50 text-7xl">
+			{{ props.error?.statusCode ?? "444" }}
+		</h1>
+		<h2 class="mb-5 text-xl opacity-50">
+			{{ props.error?.statusMessage ?? "Something went wrong" }}
+		</h2>
+		<a class="font-medium capitalize" href="/">&lt;- Return to home</a>
 	</div>
 </template>
