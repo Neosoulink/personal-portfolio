68a143bccc336440d1553489081bba8524f53da2
MNAth_ <neodarksoulink@gmail.com>
2024-02-16 19:21:32 +0300

    refactor: adds meta

--- src/app.vue
+++ src/app.vue
@@ -1,16 +1,21 @@
 <script setup lang="ts">
 useServerSeoMeta({
-	title: "nsl-me",
 	charset: "utf-8",
+	xUaCompatible: "IE=edge",
 	viewport: "width=device-width, initial-scale=1",
-	ogTitle: "Nathan Mande's website",
+	title: "Nathan Mande's Site",
 	description: "A 3D experience representing Nathan Mande's room",
+	ogSiteName: "Nathan Mande's Site",
+	ogTitle: "Nathan Mande's website",
 	ogDescription: "A 3D experience representing Nathan Mande's room",
 	ogImage: "/imgs/screenshot.png",
+	ogType: "website",
 	twitterCard: "summary_large_image",
+	twitterCreator: "https://twitter.com/nsl_nathan",
+	twitterImage: "/imgs/screenshot.png",
+	twitterDescription: "A 3D experience representing Nathan Mande's room",
 	robots: "index, skills, contact, projects, writing, notes/credits",
 	colorScheme: "dark light",
-	twitterCreator: 'https://twitter.com/nsl_nathan'
 });
 </script>
 
